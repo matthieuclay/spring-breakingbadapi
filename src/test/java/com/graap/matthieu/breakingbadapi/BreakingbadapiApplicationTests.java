0e46fe579013b2553b0f109ee5c7f313db0e2e7d
Matthieu Clay <matthieu.clay@outlook.fr>
2022-05-11 15:41:01 +0200

    :tada: initial commit

---
+++ src/test/java/com/graap/matthieu/breakingbadapi/BreakingbadapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graap.matthieu.breakingbadapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakingbadapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
